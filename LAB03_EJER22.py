168a3a18c7949e2d56994dbc451de883dead3bb2
bitsoncloud <info@bitsoncloud.com>
2024-09-30 19:28:49 -0600

    First Commit

---
+++ LAB03_EJER22.py
@@ -0,0 +1,9 @@
+#Programa: LAB02_EJER_11.py
+#Grupo: 1
+#Autores: Elmer Montoya, Elvis Aguilar, Rafael Argüello, Joseph Avilez
+#Fecha de Modificación: 23/09/2024
+#Versión de Python: 3.12
+#IDE Usada: Visual Studio Code
+#Propósito: Realizar un programa que permita sumar dos números ingresados por el usuario.
+
+print((lambda a, b: a + b)(2, 4))
